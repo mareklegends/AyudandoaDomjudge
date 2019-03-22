30db03ceb9fe3b0070f5aeb1985ab8df3d33f95c
alumno <alumno@aula1-15>
2019-03-22 18:33:36 +0100

    ya funciona todo, solo falta ordenarlos

--- AyudandoaDomjudge/src/ayudandoadomjudge/EjercicioD.java
+++ AyudandoaDomjudge/src/ayudandoadomjudge/EjercicioD.java
@@ -5,6 +5,7 @@
  */
 package ayudandoadomjudge;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -14,9 +15,14 @@ import java.util.Scanner;
 public class EjercicioD { 
     
     
+    
+  
     public static void leerCasosPrueba(){
+        ArrayList<String> vResultado = new ArrayList();
         Scanner leer = new Scanner(System.in);
         String caso = leer.nextLine();
+        
+        String guardar="";
         int numEquipos, numCasosPrueba;
         
         String aux[] = caso.split(" ");
@@ -24,7 +30,46 @@ public class EjercicioD {
         numEquipos = Integer.parseInt(aux[1]);
         numCasosPrueba = Integer.parseInt(aux[0]);
         
-        System.out.println(numCasosPrueba+" "+numEquipos);
+        //System.out.println(numCasosPrueba+" "+numEquipos);
+        
+        Scanner leer2 = new Scanner(System.in); 
+              String datos=""; 
+        for (int i = 0; i < numEquipos; i++) {
+            String equipos = leer2.nextLine();
+            String nombredelequipo="";
+            int suma = 0;
+            int contador=0;
+            
+            
+            String e[] = equipos.split(" "); 
+            
+            nombredelequipo = e[0];
+           
+            
+             for (int j = 1; j < e.length; j++) {                 
+                 if (!e[j].equalsIgnoreCase("-")) {
+                      int puntos=0;
+                      String numerosumar=e[j];
+                    puntos=Integer.parseInt(numerosumar);
+                     suma+=puntos;
+                     contador++;                          
+                 } else{
+                     j++;
+                 }
+                
+             
+              
+             
+            }
+       datos+=nombredelequipo+" "+contador+" "+suma+"\n";  
+        guardar=nombredelequipo+" "+contador+" "+suma;
+       
+       vResultado.add(guardar);
+      
+        }
+        System.out.println("/*/*/*/-/*/*/*/");
+           
+          System.out.println(datos);    
     }
     
     
